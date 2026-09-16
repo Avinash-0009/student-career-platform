e67d0162221f9a90ac9e03c53855a0ba62a851cc
Avinash-0009 <avinashjogi2005@gmail.com>
2026-09-16 14:49:46 +0530

    Added monitoring and Alerts

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -31,8 +31,8 @@ module "eks" {
       instance_types = ["t3.small"]
 
       min_size     = 1
-      max_size     = 3
-      desired_size = 2
+      max_size     = 4
+      desired_size = 3
 
       capacity_type = "ON_DEMAND"
 
